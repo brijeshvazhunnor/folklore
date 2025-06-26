1f073995d2a84cee143a29e3e3fc3b44581f85be
sharath <sharathplkn@gmail.com>
2025-06-26 23:16:16 +0530

    final

--- festapp/urls.py
+++ festapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('payment/<int:registration_id>/', views.payment, name='payment'),
+    path('brochures/', views.brochures, name='brochures'),
+
 ]
--- festapp/views.py
+++ festapp/views.py
@@ -10,20 +10,6 @@ from .forms import RegistrationForm
 def home(request):
     return render(request, 'home.html')
 
-def register(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST, request.FILES)
-        if form.is_valid():
-            instance = form.save(commit=False)
-            instance.payment_status = False  # set false until paid
-            instance.save()
+def brochures(request):
+    return render(request, 'brochures.html')
 
-            # Redirect to payment with ID
-            return redirect('payment', registration_id=instance.id)
-    else:
-        form = RegistrationForm()
-    return render(request, 'register.html', {'form': form})
-
-def payment(request, registration_id):
-    registration = Registration.objects.get(id=registration_id)
-    return render(request, 'payment.html', {'registration': registration})
